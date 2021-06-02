7f61993ee70d516b7bf38b550f0ef91a3b37a92d
NicolaiDoros <nikolae.dorosh@gmail.com>
2021-06-02 15:26:04 +0300

    release pure javascript, next release to be expected on react

---
+++ script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+//randomise the secret number & set the score & highscore
+let secretNumber = Math.trunc(Math.random() * 100) + 1;
+let score = 13;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  // case when there is no input
+  if (!guess) {
+    displayMessage("You didn't insert a number!");
+
+    // case when player wins
+  } else if (guess === secretNumber) {
+    displayMessage(" Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+
+    // case when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
+
+//next part will be to do the game on React using express on the back to save records to mongodb
